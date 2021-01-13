1ccc503816b58d5225a4afa026d630f89fdaeaf6
Sushant <sushantberiwal@gmail.com>
2021-01-13 19:59:06 +0530

    changes in checkoutComplete

--- Router/bookingRouter.js
+++ Router/bookingRouter.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { protectRoute } = require("../Controller/authController");
-const { createPaymentSession , checkoutComplete } = require("../Controller/bookingController");
-const bodyParser = require("body-parser");
+const { createPaymentSession  } = require("../Controller/bookingController");
 
-const bookingRouter = express.Router();
+
+// const bookingRouter = express.Router();
 
 bookingRouter.post("/createPaymentSession" , protectRoute , createPaymentSession);
-bookingRouter.post("/checkoutComplete" , bodyParser.raw({type: 'application/json'}) , checkoutComplete );
+// bookingRouter.post("/checkoutComplete" , bodyParser.raw({type: 'application/json'}) , checkoutComplete );
 
 module.exports = bookingRouter;
--- app.js
+++ app.js
@@ -6,9 +6,16 @@ const bookingRouter = require("./Router/bookingRouter");
 const app = express();
 const path = require("path");
 const cookieParser = require("cookie-parser"); 
+const bodyParser = require("body-parser");
 // it tracks incoming request and see if there is data in the request => the data will be fed in req.body
 app.use( express.json());
 app.use(cookieParser());
+
+
+const stripe = require("stripe");
+const stripeObj = stripe(
+  "sk_test_51I56IeA6p9e5BZD1XAbAnR51ehJPjMFOl0s06pUSfdCUoNWW9nMURKFOqyKamlIVNBgqAg3uZjTWvR3k6RoNlmz100x7EaYnPE"
+);
 
 
 
@@ -20,6 +27,36 @@ app.set("view engine" , "pug");
 
 // view path set
 app.set("views" , path.join(__dirname,"View"));
+
+
+
+app.post("/api/booking/checkoutComplete" , bodyParser.raw({type: 'application/json'})   , function(req, res) {
+  const END_POINT_KEY = process.env.END_POINT_KEY;
+  // console.log("Checkout complete ran !!");
+  // console.log("Request object");
+  // console.log(req);
+  const stripeSignature = req.headers["stripe-signature"];
+
+  console.log("endpoint key = " , END_POINT_KEY);
+  console.log("stripeSign = " , stripeSignature);
+  let event;
+  try {
+    event = stripeObj.webhooks.constructEvent(req.body, stripeSignature, END_POINT_KEY);
+    console.log("Event = " , event);
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  console.log("event object !!!");
+  console.log(event);
+  // if(event.type == "checkout.session.completed"){
+    
+  // }
+  // else{
+
+  // }
+})
+
 
 app.use("/api/booking" , bookingRouter);
 app.use("/api/plans" , planRouter);
