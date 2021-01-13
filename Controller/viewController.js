ca511950f75f9823cbf427ffba85e66e3f7f02b3
Sushant <sushantberiwal@gmail.com>
2021-01-13 19:06:42 +0530

    initial commit

---
+++ Controller/viewController.js
@@ -0,0 +1,56 @@
+const planModel = require("../Model/plansModel");
+
+
+// function getDemoPage(req, res) {
+//   // send demo page to client
+//   // res.render("demo.pug" , {title:"Demo Page" , content:"I am coming from object"});
+//   res.render("base.pug");
+// }
+
+async function getHomePage(req, res) {
+  try{
+    let plans = await planModel.find(); 
+    plans = plans.splice(0 , 3);
+    res.render("homepage.pug" , {name:req.name , plans});
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+function getProfilePage(req ,res){
+  res.render("profilePage.pug" , {user : req.user});
+}
+
+function getResetPasswordPage(req , res){
+  res.render("resetPassword.pug" , {name:req.name});
+}
+
+function getLoginPage(req, res) {
+  res.render("login.pug" , {name:req.name});
+}
+
+function getSignUpPage(req , res){
+    res.render("signup.pug" , {name:req.name});
+}
+
+// single plan leke aayega
+
+
+async function getPlansPage(req , res){
+    try{
+        let plans = await planModel.find(); 
+        console.log(plans);
+        res.render("plans.pug" ,{name:req.name , plans:plans})
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+module.exports.getHomePage = getHomePage;
+module.exports.getLoginPage = getLoginPage;
+module.exports.getSignUpPage = getSignUpPage;
+module.exports.getPlansPage = getPlansPage;
+module.exports.getResetPasswordPage = getResetPasswordPage;
+module.exports.getProfilePage = getProfilePage;
---
+++ Model/bookingModel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+// const { DB_LINK } = require("../config/secrets");
+
+
+const DB_LINK = process.env.DB_LINK;
+mongoose
+  .connect(
+    DB_LINK,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then((db) => {
+    console.log("Connected to db !!!");
+  });
+
+//bookedPlan Schema
+const bookedPlanSchema = new mongoose.Schema({
+    planId : {
+        type:String,
+        required:true
+    },
+    planName : {
+        type:String,
+        required:true
+    },
+    currentPrice : {
+        type:Number,
+        required:true
+    },
+    bookedOn : {
+        type:String,
+        default:Date.now()
+    }
+})
+
+// booking Schema
+const bookingSchema = new mongoose.Schema({
+    userId :{
+        type:String,
+        required:true
+    },
+    bookedPlans:{
+        type:[bookedPlanSchema],
+        required:true
+    }
+})
+
+
+const bookingModel = mongoose.model("bookingcollection" , bookingSchema);
+module.exports = bookingModel;
+
